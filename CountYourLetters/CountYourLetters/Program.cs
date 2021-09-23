c4f25592cd98709c2b7c54e96455200cf869bf7a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-23 07:41:39 -0400

    completed the lesson

--- CountYourLetters/CountYourLetters/Program.cs
+++ CountYourLetters/CountYourLetters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CountYourLetters
 {
@@ -6,7 +7,35 @@ namespace CountYourLetters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            Console.WriteLine("Type in a phrase on a single line: ");
+            string userInput = Console.ReadLine();
+
+            foreach (char c in userInput)
+            {
+                char currentChar;
+                currentChar = c;
+                currentChar = char.ToUpper(c);
+
+                int currrentNumber;
+
+                if (counts.TryGetValue(currentChar, out currrentNumber))
+                {
+                    counts[currentChar] = currrentNumber + 1;
+                    Console.WriteLine($"Already counted '{currentChar}', {currrentNumber} times, adding it once more.");
+                }
+                else
+                {
+                    counts.Add(currentChar, 1);
+                    Console.WriteLine($"'{currentChar}' is new! Adding it to the list and setting it to 1!");
+                }
+            }
+
+            Console.WriteLine("************** Counting is Complete!!!**********");
+            foreach (var data in counts)
+            {
+                Console.WriteLine($"'{data.Key}' was found {data.Value} time(s)");
+            }
         }
     }
 }
