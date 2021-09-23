ab25a3894b12d42e70567134a8189176606d0e50
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-23 07:44:20 -0400

    added prior lesson programs to the repo

---
+++ EmployeeInfo/EmployeeInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Lucinda Potter";
+            string birthDate = "June 24, 1992";
+            string workPhoneNumber = "312-982-1010";
+            string eMail = "ewoz@woz-u.com";
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(birthDate);
+            Console.WriteLine(workPhoneNumber);
+            Console.WriteLine(eMail);
+        }
+    }
+}
---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
