85080601047ba2915e4ff1a5bbb35c4d9c33bd15
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-23 07:24:53 -0400

    created Problem 2: Hard

---
+++ CountYourLetters/CountYourLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountYourLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
