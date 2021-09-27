cb9388fe6407c2471506007ea223fdfd1e1b1ff1
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-27 16:14:06 -0400

    added HandsOnPracticeProblem1

---
+++ HandsOnPracticeProblem1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HandsOnPracticeProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Project 1
+
+            Sale s1 = new Sale("One", 1000, .10M);
+            Sale s2 = new Sale("One", 500, .05M);
+            Sale s3 = new Sale("Two", 2500, .10M);
+            Sale s4 = new Sale("Two", 3000);
+            Sale s5 = new Sale("One", 1500, .083M);
+            Sale s6 = new Sale("Two", 5500, .062M);
+
+            try
+            {
+                Console.WriteLine($"Employee: {s1.Employee, 10}   Sales: {s1.SalesAmount, 15}   Commission: {s1.Commission, 10}");
+                Console.WriteLine($"Employee: {s2.Employee, 10}   Sales: {s2.SalesAmount, 15}   Commission: {s2.Commission, 10}");
+                Console.WriteLine($"Employee: {s3.Employee, 10}   Sales: {s3.SalesAmount, 15}   Commission: {s3.Commission, 10}");
+                Console.WriteLine($"Employee: {s4.Employee, 10}   Sales: {s4.SalesAmount, 15}   Commission: {s4.Commission, 10}");
+                Console.WriteLine($"Employee: {s5.Employee, 10}   Sales: {s5.SalesAmount, 15}   Commission: {s5.Commission, 10}");
+                Console.WriteLine($"Employee: {s6.Employee, 10}   Sales: {s6.SalesAmount, 15}   Commission: {s6.Commission, 10}");
+                Console.WriteLine("=============================================================================================");
+                // Console.WriteLine(SalesTotal(s1.SalesAmount, s1.Employee, s2.SalesAmount, s2.Employee));
+                // Console.WriteLine(SalesTotal(s3.SalesAmount, s3.Employee, s4.SalesAmount, s4.Employee));
+                // Console.WriteLine(SalesTotal(s2.SalesAmount, s2.Employee, s3.SalesAmount, s3.Employee));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Project 2
+
+            // This code block reads a test file that you will need to create
+            // To create a text file:
+            // Right-click on the project name
+            // hover over add
+            // click on "New Item"
+            // On the left hand side of the box, choose "General"; select "Text File" from the list; name your text file and click "Add"
+            // Add the information from the project outline to the text file and save it
+
+
+            // This line of code reads from the text file you created
+            System.IO.TextReader reader = System.IO.File.OpenText("../../../sales.txt");
+            string line;
+            // creating a new list
+            List<Sale> salesData = new List<Sale>();
+            
+            // The while statement reads through the file and adds the line to your list
+            while (!string.IsNullOrWhiteSpace(line = reader.ReadLine()))
+            {
+                try
+                {
+                    salesData.Add(new Sale(line));
+                }
+                // this line will throw an exception if the line is not readable
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            // this closes the reader
+            reader.Close();
+
+            // This code block prints the list to the console
+            for (int i = 0; i < salesData.Count; i++)
+            {
+                Console.WriteLine($"{i,3}: {salesData[i]}");
+                
+            }
+            Console.WriteLine($"*********************************************************************************************");
+            while (true) ;
+            
+
+            
+
+        }
+        /*
+        public static string SalesTotal(decimal a, string employeeA, decimal b, string employeeB)
+        {
+            Sale s1 = new Sale("One", 1000, .10M);
+            Sale s2 = new Sale("One", 500, .05M);
+            Sale s3 = new Sale("Two", 2500, .10M);
+            Sale s4 = new Sale("Two", 3000);
+            Sale s5 = new Sale("One", 1500, .083M);
+            Sale s6 = new Sale("Two", 5500, .062M);
+
+            string message = "";
+            if (employeeA == employeeB)
+            {
+                decimal salesAmountSum = a + b;
+                message = salesAmountSum.ToString();
+            }
+            else
+            {
+                message = "Can only add sale objects if the employee is the same. In this case left = \"One\" and Right = \"Two\"";
+            }
+
+            return message;
+        }
+        */
+
+        
+        
+
+
+    }
+
+}
---
+++ HandsOnPracticeProblem1/Sale.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnPracticeProblem1
+{
+    // Project 1
+    public class Sale
+    {
+        public string Employee { get; set; }
+        public decimal SalesAmount { get; set; }
+        private decimal _commissionRate { get; set; }
+        public decimal Commission
+        {
+            get { return SalesAmount * _commissionRate;  }
+        }
+
+        public Sale(string Employee, decimal SalesAmount, decimal _commissionRate)
+        {
+            this.Employee = Employee;
+            this.SalesAmount = SalesAmount;
+            this._commissionRate = _commissionRate;
+        }
+
+        public Sale(string Employee, decimal SalesAmount)
+        {
+            this.Employee = Employee;
+            this.SalesAmount = SalesAmount;
+            this._commissionRate = .03m;
+        }
+
+        public string SalesTotal(decimal a, decimal b)
+        {
+            string message = "";
+            if (Employee == Employee)
+            {
+                decimal salesAmountSum = SalesAmount + SalesAmount;
+                message = salesAmountSum.ToString();
+            }
+            else
+            {
+                message = "Can only add sale objects if the employee is the same. In this case left = \"One\" and Right = \"Two\"";
+            }
+
+            return message;
+        }
+
+        public Sale(string csv)
+        {
+            
+          
+            string[] data = csv.Split('|');
+            if (3 != data.Length)
+            {
+                throw new Exception($"CSV entry '{csv}' does not contain the correct number of commas");
+            }
+            Employee = data[0];
+            decimal temp;
+            if (!decimal.TryParse(data[1], out temp))
+            {
+                throw new Exception($"CSV entry '{csv}' is not recoginizable as a decimal.");
+            }
+            SalesAmount = temp;
+            if (!decimal.TryParse(data[2], out temp))
+            {
+                throw new Exception($"CSV entry '{csv}' is not recoginizable as a decimal.");
+            }
+            _commissionRate = temp;
+         
+
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {Employee,10} Sale Amount: {SalesAmount,15} Rate: {_commissionRate,10} Commission: {Commission}";
+        }
+
+        public static Sale operator +(Sale left, Sale right)
+        {
+            if (left.Employee != right.Employee)
+            {
+                throw new Exception($"Can only add sale object if Employee is the same. This means '{left.Employee}' = '{right.Employee}'.");
+            }
+            decimal totalCommission = left.Commission + right.Commission;
+            decimal totalSale = left.SalesAmount + right.SalesAmount;
+            decimal combinedRate = totalCommission / totalSale;
+            return new Sale(left.Employee, totalSale, combinedRate);
+        }
+    }   
+}
