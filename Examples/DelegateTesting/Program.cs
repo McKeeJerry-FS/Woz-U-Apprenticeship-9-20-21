3255c51fe521fd2c0fea8c7f4a71fd9210e4d370
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-28 14:40:00 -0400

    Lesson 8 - Hands On Practice - Debugging

---
+++ Examples/DelegateTesting/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace DelegateTesting
+{
+    class Hello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello There!");
+        }
+
+        public int ScreamHello(int i)
+        {
+            Console.WriteLine($"HELLO THERE {i} times");
+            return 2 * i;
+        }
+    }
+
+    class Goodbye
+    {
+        public string Name { get; set; }
+
+        public void SayGoodbye()
+        {
+            Console.WriteLine($"Goodbye {Name}");
+
+        }
+        public int ScreamGoodbye(int c)
+        {
+            Console.WriteLine($"GOODBYE {Name.ToUpper()} {c} times!");
+            return 10 * c;
+        }
+    }
+
+    class Ouch
+    {
+        public string Perp { get; set; }
+        public string Victim { get; set; }
+
+        public void SayOuch()
+        {
+            Console.WriteLine($"{Perp} breathes on {Victim}");
+        }
+
+        public int ScreamOuch(int count)
+        {
+            for (int i = 1; i < count; i++)
+            {
+                Console.WriteLine($"{Perp} Breathes on {Victim} time {i} of {count}");
+            }
+            return count;
+        }
+    }
+
+    delegate void myDelegate();
+    delegate int myOtherDelegate(int count);
+    delegate void stillAnotherDelegate(int current, int total);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare reference variables
+            Hello h;
+            Goodbye g1;
+            Goodbye g2;
+            Ouch o;
+
+            // Step 2: Attach references to instances
+            h = new Hello();
+            g1 = new Goodbye();
+            g2 = new Goodbye();
+            o = new Ouch();
+
+            // Step 3: Configure Instances
+            g1.Name = "Brian";
+            g2.Name = "Carol";
+            o.Perp = "Daniel";
+            o.Victim = "Lydia";
+
+            // Step 4: Exercise the intances
+            h.SayHello();
+            h.ScreamHello(3);
+            g1.SayGoodbye();
+            g2.SayGoodbye();
+            g1.ScreamGoodbye(4);
+            g2.ScreamGoodbye(5);
+            o.SayOuch();
+            o.ScreamOuch(5);
+        }
+    }
+}
---
+++ Lesson_9_Hands_On_Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson9HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 4, 7, 29, 43, 12, 20, 88, 121 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
