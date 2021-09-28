6fde304cc6e0c0515803631fddf830558fa63f6b
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-28 08:51:46 -0400

    completed the Lesson 6 Assignment

--- Lesson_7_Hands_On_Practice_Inheritance/Program.cs
+++ Lesson_7_Hands_On_Practice_Inheritance/Program.cs
@@ -6,7 +6,19 @@ namespace Lesson_7_Hands_On_Practice_Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creating an Engineer object
+            Engineer engineer1 = new Engineer("Peter Parker", 48000, "May 6, 2016", "New York University");
+            engineer1.getName();
+            engineer1.getSalary();
+            engineer1.hiredDate();
+
+            Console.WriteLine("========================================================================");
+
+            // Creating a software Engineer Object
+            SoftwareEngineer softwareEngineer1 = new SoftwareEngineer("Tony Stark", 120000, "May 2, 2008", "Andover University");
+            softwareEngineer1.getName();
+            softwareEngineer1.getSalary();
+            softwareEngineer1.hiredDate();
         }
     }
 }
