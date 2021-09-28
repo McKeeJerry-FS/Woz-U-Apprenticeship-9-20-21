0c0e6f805f99921a57bdd7303ddbd82ebe32a6af
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-28 10:26:40 -0400

    added a new sample
    this program was for after practice discussion; the program utilizes a static Validation Method to protect a program from crashing due to invalid responses

---
+++ DailyPractice/DailyPractice 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DailyPractice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // Get two number
+            Console.WriteLine("Type in a number");
+            int userInput = IntegerValidation(Console.ReadLine());
+            Console.WriteLine("Type in a number");
+            int userInput2 = IntegerValidation(Console.ReadLine());
+            // Add two numbers
+            int sum = userInput + userInput2;
+            Console.WriteLine(sum);
+            
+        }
+
+        // Integer Validation Method
+        public static int IntegerValidation(string input)
+        {
+            int userInput;
+            while (!(int.TryParse(input, out userInput)))
+            {
+                //Tell the user the error
+                Console.WriteLine("\r\n Invalid Entry");
+                //Reprompt
+                Console.WriteLine("Please type in a valid number.");
+                //Recapture
+                input = Console.ReadLine();
+            }
+            return userInput;
+        }
+        // end IntegerValidation()
+    }
+}
