f8684ca2ccfef922246a4ecefb54a935ed1e5782
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-28 08:55:15 -0400

    updates

---
+++ Lesson_6_Hands_On_Practice_Inheritance/Engineer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_Hands_On_Practice_Inheritance
+{
+    class Engineer : Employee
+    {
+        // class field
+        string schoolAttended;
+
+        // constructor that inherits properties from the Employee class
+        public Engineer(string name, int salary, string hireDate, string schoolAttened) : base(name, salary, hireDate)
+        {
+            this.schoolAttended = schoolAttended;
+        }
+    }
+}
---
+++ Lesson_6_Hands_On_Practice_Inheritance/SoftwareEngineer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_Hands_On_Practice_Inheritance
+{
+    class SoftwareEngineer : Engineer
+    {
+        // Constructor that inherits properties from the Engineering class
+        public SoftwareEngineer (string name, int salary, string hireDate, string schoolAttended) : base(name, salary, hireDate, schoolAttended)
+        {
+        }
+
+        // Override Method to get the Employee's Salary
+        public override void getSalary()
+        {
+            Console.WriteLine($"Salary: Sorry, this employee's salary is private.");
+        }
+    }
+}
