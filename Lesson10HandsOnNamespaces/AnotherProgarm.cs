84cfecdd6498cf9f0a47061a5b0c8dd644f5b131
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-29 09:12:35 -0400

    adding Lesson 9 program and creating Final Project

---
+++ Lesson10HandsOnNamespaces/AnotherProgarm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNamespace
+{
+    class AnotherProgarm
+    {
+        public static void PrintText()
+        {
+            Console.WriteLine("Hello Earth!");
+        }
+    }
+    
+}
+
+namespace aThirdNamespace
+{
+    class newProgram
+    {
+        public static void PrintThisText()
+        {
+            Console.WriteLine("Hello from A Third Namespace!");
+        }
+    }
+}
---
+++ Lesson10HandsOnNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using myNamespace;
+using aThirdNamespace;
+
+namespace Lesson10HandsOnNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AnotherProgarm.PrintText();
+            newProgram.PrintThisText();
+        }
+    }
+}
